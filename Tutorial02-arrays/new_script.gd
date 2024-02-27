a035e80a7f01275405dfad8f6fa6d0b7709955e8
@mzadante <mzadantemartinez@gmail.com>
2024-02-27 11:07:37 -0300

    comenzando

--- Tutorial02-arrays/new_script.gd
+++ Tutorial02-arrays/new_script.gd
@@ -1,5 +1,11 @@
 extends Node
 # continuacion del tutorial
 func _ready():
-	print("hello world")
+	"""
+	Para entender los Arrays en gd script, vamos a ejemplificar imaginando
+	que tenemos un inventario o un bolso en nuestro juego al cual le vamos
+	a ir agregando objetos, aqui veremos como añadir o quitar dichos objetos
+	ademas de recorerlos y aplicar lo visto anteriormente como bucles y más
+	"""
+	
 
