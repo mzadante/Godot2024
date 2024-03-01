669bb453a893c6ebe83932478c99c4db4ebb8e1e
@mzadante <mzadantemartinez@gmail.com>
2024-02-29 21:57:37 -0300

    Tutorial Ambito de las variables
    05-gdscript-Ambito_de_las_variables

---
+++ 05-gdscript-Ambito_de_las_variables/new_script.gd
@@ -0,0 +1,47 @@
+extends Node
+## colocando la variable aqui podra ser vista por ambas funciones de abajo
+## tanto de la _ready() como la de recolectar
+var bolso = {madera = 1, piedra = 3, oro = 1} #variable sin identar
+
+func _ready():
+	"""
+	AMBITO DE LAS VARIABLES:
+	Por ejemplo para una comprención facil, vamos a imaginar que queremos
+	crer una funcion que nos sirva para recoger materiales y agregarlos,
+	facilmente en nuestro bolso	  
+	"""
+	#var bolso = {madera = 1, piedra = 3, oro = 1} #comentada para evitar error
+	var otro_bolso = {madera = 1, piedra = 3, oro = 1}
+	recolectar("piedra")
+	recolectar_con_argumento(otro_bolso, "oro") #pasando el argumento y el material
+	print(bolso)
+	print(otro_bolso)
+	
+	
+	
+## Creamos la función recolectar()
+func recolectar(material, cantidad = 1):
+	if bolso.has(material): # si existe el material que agregue la cantidad establecida
+		bolso[material] += cantidad	
+	else: # si no esta el material que agregue la cantidad por defecto
+		bolso[material] = cantidad 
+"""
+VARIABLES GLOBALES: 
+Esta función recolectar que tenemos arriba no funcionara porque, el contenido
+de la variable bolso se encuentra fuera del escope de la funcion, es decir solo
+puede ser vista esta variable por la función _ready() y no por la función
+recolectar, para solucionar el error deberemos colocar la variable bolso arriba
+de todo fuera de la función _ready()  y sin identación , es decir a la misma
+sangria que dichas funciones. 
+De esta forma quedaria establecida como una variable Global
+Para arrays y diccionarios: (POR REFERENCIA)
+Otra solucion para que dicha función no hubiera tenido ese problema, es haberla
+pasado por ARGUMENTO, es decir que la variable recolectar la hubiera recibido
+como parametro, vea el ejemplo de abajo:
+	Recordar que esto funciona en los tipos complejos como diccionarios o arrays
+"""		
+func recolectar_con_argumento(otro_bolso, material, cantidad = 1):
+	if otro_bolso.has(material): # si existe el material que agregue la cantidad establecida
+		otro_bolso[material] += cantidad	
+	else: # si no esta el material que agregue la cantidad por defecto
+		otro_bolso[material] = cantidad 
